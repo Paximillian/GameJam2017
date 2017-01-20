16be7d7689fb91ee935bb38234899e6f1ce4150f
NTDOM\mor <paximillian@gmail.com>
2017-01-20 18:59:56 +0200

    - Finally started setting up HoloLens

---
+++ GGJ2017/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    [SerializeField]
+    private TextToSpeechManager m_Tts;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Tts.SpeakText("Please scan your area and find a table");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<GameObject> tablePlanes = SurfaceMeshesToPlanes.Instance.GetActivePlanes(PlaneTypes.Table);
+    }
+}
